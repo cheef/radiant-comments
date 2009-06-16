4d5195f9638b1f34611871824ffd1b7878970e8d
Gerrit Kaiser <gerrit@gerritkaiser.de>
2009-06-16 16:14:50 +0100

    moved modifications to Page class from extension initializer into their own module

--- comments_extension.rb
+++ comments_extension.rb
@@ -23,19 +23,8 @@ class CommentsExtension < Radiant::Extension
   
   def activate
     Page.send :include, CommentTags
+    Page.send :include, Commentable
     Comment
-    
-    Page.class_eval do
-      has_many :comments, :dependent => :destroy, :order => "created_at ASC"
-      has_many :approved_comments, :class_name => "Comment", :conditions => "comments.approved_at IS NOT NULL", :order => "created_at ASC"
-      has_many :unapproved_comments, :class_name => "Comment", :conditions => "comments.approved_at IS NULL", :order => "created_at ASC"
-      attr_accessor :last_comment
-      attr_accessor :selected_comment
-      
-      def has_visible_comments?
-        !(approved_comments.empty? && selected_comment.nil?)
-      end
-    end
     
     if admin.respond_to? :page
       admin.page.edit.add :parts_bottom, "edit_comments_enabled", :before => "edit_timestamp"
